9762de023a40d83c490a7856eed450b5afdfd7ef
Ingo Albrecht <commit@promovicz.org>
2020-09-28 12:58:06 +0200

    Fix action dump

--- clish/action/action_dump.c
+++ clish/action/action_dump.c
@@ -13,10 +13,14 @@ void clish_action_dump(const clish_action_t *this)
 	lub_dump_printf("action(%p)\n", this);
 	lub_dump_indent();
 
-	lub_dump_printf("script  : %s\n", LUB_DUMP_STR(this->script));
-	lub_dump_printf("builtin : %s\n",
+	lub_dump_printf("script      : %s\n", LUB_DUMP_STR(this->script));
+	lub_dump_printf("builtin     : %s\n",
 		this->builtin ? clish_sym__get_name(this->builtin) : LUB_DUMP_NULL );
-	lub_dump_printf("shebang : %s\n", LUB_DUMP_STR(this->shebang));
+	lub_dump_printf("shebang     : %s\n", LUB_DUMP_STR(this->shebang));
+	lub_dump_printf("lock        : %s\n", LUB_DUMP_BOOL(this->lock));
+	lub_dump_printf("interrupt   : %s\n", LUB_DUMP_BOOL(this->interrupt));
+	lub_dump_printf("interactive : %s\n", LUB_DUMP_BOOL(this->interactive));
+	lub_dump_printf("permanent   : %s\n", LUB_DUMP_BOOL(this->permanent));
 
 	lub_dump_undent();
 }
