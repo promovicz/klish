76f6b864d9ea8c125fa890c6fb3b98110cf8eaf4
Serj Kalichev <serj.kalichev@gmail.com>
2020-10-13 17:10:06 +0300

    Tri-state boolean tri_t type

--- lub/types.h
+++ lub/types.h
@@ -20,5 +20,16 @@ typedef enum {
 	BOOL_TRUE
 } bool_t;
 
+/**
+ * A tri-state boolean. The possible values are
+ * TRI_FALSE, TRI_TRUE, TRI_UNDEFINED.
+ */
+typedef enum {
+       TRI_UNDEFINED = -1,
+       TRI_FALSE = 0,
+       TRI_TRUE = 1
+} tri_t;
+
+
 /** @} */
 #endif				/* _lub_types_h */
